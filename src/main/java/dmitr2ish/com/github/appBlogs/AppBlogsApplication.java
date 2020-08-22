ab644371a3680f17f7379d0b737fa8d56498027b
dmitr2ish <dmitr2ish@yandex.ru>
2020-08-22 16:46:16 +0500

    - init

---
+++ src/main/java/dmitr2ish/com/github/appBlogs/AppBlogsApplication.java
@@ -0,0 +1,13 @@
+package dmitr2ish.com.github.appBlogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBlogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppBlogsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dmitr2ish/com/github/appBlogs/AppBlogsApplicationTests.java
@@ -0,0 +1,13 @@
+package dmitr2ish.com.github.appBlogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBlogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
